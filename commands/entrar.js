614f2a7beb5c5af5fe359bd6133e7cd7997ebc69
abnerbenke222-cmd <abnerbenke222@gmail.com>
2026-02-19 09:20:27 -0300

    Update entrar.js

--- commands/entrar.js
+++ commands/entrar.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
+const fila = require("../database/fila.json");
+
+module.exports = {
+  name: "entrar",
+  execute(interaction) {
+
+    if (fila.fila.includes(interaction.user.id)) {
+      return interaction.reply({ content: "❌ Você já está na fila.", ephemeral: true });
+    }
+
+    fila.fila.push(interaction.user.id);
+
+    fs.writeFileSync("./database/fila.json", JSON.stringify(fila, null, 2));
+
+    interaction.reply({ content: "✅ Você entrou na fila de mediadores!", ephemeral: true });
+  }
+};
 
