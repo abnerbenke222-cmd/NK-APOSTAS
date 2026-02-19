a3608b045858786022d8bef7d094632fd7b349ef
abnerbenke222-cmd <abnerbenke222@gmail.com>
2026-02-19 09:20:47 -0300

    Update sair.js

--- commands/sair.js
+++ commands/sair.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
+const fila = require("../database/fila.json");
+
+module.exports = {
+  name: "sair",
+  execute(interaction) {
+
+    if (!fila.fila.includes(interaction.user.id)) {
+      return interaction.reply({ content: "❌ Você não está na fila.", ephemeral: true });
+    }
+
+    fila.fila = fila.fila.filter(id => id !== interaction.user.id);
+
+    fs.writeFileSync("./database/fila.json", JSON.stringify(fila, null, 2));
+
+    interaction.reply({ content: "🚪 Você saiu da fila!", ephemeral: true });
+  }
+};
 
